c7a5e56c3a9061acb208359eff19def46df3d6a0
Wei Yang <wei.yang@rwth-aachen.de>
2023-04-09 18:39:27 +0200

    Update hrwos_support.

---
+++ hrwros_ws/src/hrwros/hrwros_support/launch/visualize_hrwros.launch.py
@@ -0,0 +1,58 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch.conditions import IfCondition, UnlessCondition
+from launch import LaunchDescription
+from launch.substitutions import Command, LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
+from launch_ros.actions import Node
+
+
+
+def generate_launch_description():
+    pkg_share = get_package_share_directory('hrwros_support')
+    default_model_path = os.path.join(pkg_share, 'urdf/hrwros.xacro')
+    default_rviz_config_path = os.path.join(pkg_share, 'config/hrwros.rviz')
+    config = os.path.join(
+        pkg_share,
+        'config',
+        'params.yaml'
+    )
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        parameters=[{'robot_description': Command(['xacro ', LaunchConfiguration('model')])}],
+    )
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        parameters = [config],
+        condition=UnlessCondition(LaunchConfiguration('gui')),
+    )
+    joint_state_publisher_gui_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui',
+        parameters = [config],
+        condition=IfCondition(LaunchConfiguration('gui')),
+    )
+    rviz_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', LaunchConfiguration('rvizconfig')],
+    )
+
+    return LaunchDescription([
+        DeclareLaunchArgument(name='gui', default_value='False',
+            description='Flag to enable joint_state_publisher_gui'),
+        DeclareLaunchArgument(name='model', default_value=default_model_path,
+           description='Absolute path to robot urdf file'),
+        DeclareLaunchArgument(name='rvizconfig', default_value=default_rviz_config_path,
+            description='Absolute path to rviz config file'),
+        joint_state_publisher_node,
+        joint_state_publisher_gui_node,
+        robot_state_publisher_node,
+        rviz_node
+    ])
